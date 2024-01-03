35be8ee68a2cbc81d3d670f90365b1f42597bfc7
saurabhsingh <saurabh623a@gmail.com>
2024-01-03 22:12:21 +0530

    recursion added

---
+++ recursion/Print1toN.java
@@ -0,0 +1,12 @@
+package recursion;
+
+public class Print1toN {
+    public static void print1ToN(int n) {
+
+        if (n == 0) {
+            return ;
+        }
+        print1ToN(n - 1);
+        System.out.print(n +" ");
+    }
+}
---
+++ recursion/PrintNto1.java
@@ -0,0 +1,11 @@
+package recursion;
+
+public class PrintNto1 {
+    public static void printNto1(int n) {
+        if (n == 0) {
+            return ;
+        }
+        System.out.print(n +" ");
+        printNto1(n - 1);
+    }
+}
