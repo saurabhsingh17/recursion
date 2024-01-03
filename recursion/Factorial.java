79d86587957ea69088ebfebc86003be5c3b8fe37
saurabhsingh <saurabh623a@gmail.com>
2024-01-03 22:43:01 +0530

    recursion added

---
+++ recursion/Factorial.java
@@ -0,0 +1,16 @@
+package recursion;
+
+import java.math.BigInteger;
+
+public class Factorial {
+    // 1*
+    public static BigInteger factorial(int n){
+        if (n == 0)
+            return BigInteger.valueOf(1);
+        return (factorial(n-1).multiply(BigInteger.valueOf(n)));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(50));
+    }
+}
