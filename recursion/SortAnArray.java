4d5136b787268dd10c09dee7743867ac1869d700
saurabhsingh <saurabh623a@gmail.com>
2024-01-04 23:37:28 +0530

    recursion added

---
+++ recursion/SortAnArray.java
@@ -0,0 +1,13 @@
+package recursion;
+
+import java.util.ArrayList;
+
+public class SortAnArray {
+    public static void sort(ArrayList<Integer> arr){
+        if (arr.isEmpty()){
+            return;
+        }
+
+
+    }
+}
