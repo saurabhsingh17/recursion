a7069b480f2a8240c60ab7b121e58e837c833a49
saurabhsingh <saurabh623a@gmail.com>
2024-01-06 18:58:16 +0530

    recursion added

---
+++ recursion/DeleteMiddleElementOfStack.java
@@ -0,0 +1,31 @@
+package recursion;
+
+import java.util.Stack;
+
+public class DeleteMiddleElementOfStack {
+    public static void delete(Stack<Integer> stack, int k){
+        if(k == 1) {
+            stack.pop();
+            return;
+        }
+        int val = stack.pop();
+        delete(stack, k-1);
+        stack.push(val);
+
+    }
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.push(6);
+        stack.push(7);
+        //stack.push(8);
+        System.out.println(stack);
+
+        delete(stack,(stack.size()/2)+1);
+        System.out.println(stack);
+    }
+}
---
+++ recursion/SortAStack.java
@@ -0,0 +1,37 @@
+package recursion;
+
+import java.util.Stack;
+
+public class SortAStack {
+    public static void sortAStack(Stack<Integer> stack){
+        if (stack.isEmpty())
+            return;
+        int lastElement = stack.pop();
+        sortAStack(stack);
+        insertInStack(stack,lastElement);
+    }
+    public static void insertInStack(Stack<Integer>stack,int val){
+        if(stack.isEmpty() || stack.peek() >= val){
+            stack.push(val);
+            return;
+        }
+        int temp = stack.pop();
+        insertInStack(stack,val);
+        stack.push(temp);
+    }
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(5);
+        stack.push(3);
+        stack.push(2);
+        stack.push(2);
+        stack.push(2);
+        stack.push(2);
+        stack.push(2);
+        stack.push(2);
+        sortAStack(stack);
+        System.out.println(stack);
+    }
+
+}
+
