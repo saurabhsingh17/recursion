7af04ccf7bf28d2a7cb6aab7064099bb14f50995
saurabhsingh <saurabh623a@gmail.com>
2024-01-06 12:16:44 +0530

    recursion added

--- recursion/SortAnArray.java
+++ recursion/SortAnArray.java
@@ -7,7 +7,37 @@ public class SortAnArray {
         if (arr.isEmpty()){
             return;
         }
+        int val = arr.get(arr.size()-1);
+        arr.remove(arr.size()-1);
+        sort(arr);
+        insert(arr,val);
+    }
+    public static void insert(ArrayList<Integer> arr,int val){
+        if ( arr.isEmpty() || arr.get(arr.size()-1) <= val){
+            arr.add(val);
+            return;
+        }
+        int temp = arr.get(arr.size()-1);
+        arr.remove(arr.size()-1);
+        insert(arr, val);
+        arr.add(temp);
+    }
 
-
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(2);
+        arr.add(1);
+        arr.add(0);
+        arr.add(2);
+        arr.add(10);
+        arr.add(20);
+        arr.add(29);
+        arr.add(12);
+        arr.add(0);
+        arr.add(2);
+        arr.add(111);
+        arr.add(0);
+        sort(arr);
+        System.out.println(arr);;
     }
 }
