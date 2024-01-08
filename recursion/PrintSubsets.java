e20e4e5c12f1bb8c696ec76730a8b6fb726f7967
saurabhsingh <saurabh623a@gmail.com>
2024-01-08 23:11:51 +0530

    recursion added

---
+++ recursion/PrintSubsets.java
@@ -0,0 +1,29 @@
+package recursion;
+
+public class PrintSubsets {
+    static int count = 0;
+
+    static void printSubsets(String in, String out) {
+
+        if (in.isEmpty()) {
+            count++;
+            System.out.println(out);
+            return;
+        }
+        String out1 = out;
+        String out2 = out;
+        out2 += in.charAt(0);
+        in = in.substring(1);
+
+        printSubsets(in, out1);
+        printSubsets(in, out2);
+
+    }
+
+    public static void main(String[] args) {
+        String in = "abab";
+        String out = "";
+        printSubsets(in, out);
+        System.out.println(count);
+    }
+}
