d24c12fa6b43c10b9d699dc4c8bf6ec71db7c389
saurabhsingh <saurabh623a@gmail.com>
2024-01-09 22:01:42 +0530

    recursion added

---
+++ recursion/Codechef1.java
@@ -0,0 +1,20 @@
+package recursion;
+
+public class Codechef1 {
+    static String printDays(int n){
+        return switch (n){
+            case 1 -> "Monday";
+            case 2 -> "Tuesday";
+            case 3 -> "Wednesday";
+            case 4 -> "Thursday";
+            case 5 -> "Friday";
+            case 6 -> "Saturday";
+            case 7 -> "Sunday";
+            default -> throw new IllegalStateException("Unexpected value: " + n);
+        };
+    }
+
+    public static void main(String[] args) {
+        System.out.println(printDays(1));
+    }
+}
---
+++ recursion/PrintSubsetsInList.java
@@ -0,0 +1,37 @@
+package recursion;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class PrintSubsetsInList {
+    static int count = 0;
+    static HashSet<ArrayList<Integer>> result = new HashSet<>();
+    static HashSet<ArrayList<Integer>> printSubsets(ArrayList<Integer> in, ArrayList<Integer> out) {
+
+        if (in.isEmpty()) {
+            count++;
+            result.add(out);
+            return result;
+        }
+        ArrayList<Integer> out1 = new ArrayList<>(out);
+        ArrayList<Integer> out2 = new ArrayList<>(out);
+        out2.add(in.get(0));
+        in.remove(0);
+
+        printSubsets(new ArrayList<>(in), out1);
+        printSubsets(new ArrayList<>(in), out2);
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> in = new ArrayList<>();
+        in.add(1);
+        in.add(2);
+//        in.add(3);
+        ArrayList<Integer> out = new ArrayList<>();
+        printSubsets(in, out);
+        System.out.println(printSubsets(in,out));
+    }
+}
