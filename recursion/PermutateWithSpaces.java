9bd34742a8caf37daf4fe4d459c36728abcfd7b4
saurabhsingh <saurabh623a@gmail.com>
2024-01-10 21:48:13 +0530

    recursion added

---
+++ recursion/PermutateWithSpaces.java
@@ -0,0 +1,28 @@
+package recursion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PermutateWithSpaces {
+    static List<String> result = new ArrayList<>();
+    static List<String> permutate(String input, String output){
+        if (input.isEmpty()){
+            result.add(output);
+            return result;
+        }
+        String out1  = output;
+        String out2 = output;
+
+            out1 += "_"+input.charAt(0);
+            out2 += input.charAt(0);
+
+        input = input.substring(1);
+        permutate((input), out1);
+        permutate((input), out2);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(permutate("abc","a"));
+    }
+}
