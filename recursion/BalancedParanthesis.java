de6ff63294d9c90af5c23b626a011d376d14a68f
saurabhsingh <saurabh623a@gmail.com>
2024-01-11 21:55:20 +0530

    recursion added

---
+++ recursion/BalancedParanthesis.java
@@ -0,0 +1,35 @@
+package recursion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BalancedParanthesis {
+
+    static List<String> result = new ArrayList<>();
+    static List<String> generate(int left,int right, String output) {
+        if (left == 0  && right == 0){
+            result.add(output);
+            return result;
+        }
+        if(left != 0) {
+            String out1 = output;
+            out1 +="{";
+
+            generate(left-1, right, out1);
+
+        }
+        if (right > left){
+            String out2 = output;
+            out2+="}";
+            generate(left,right-1,out2);
+
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(generate(10,10,""));
+    }
+
+}
---
+++ recursion/PermutationWithCaseChange.java
@@ -0,0 +1,34 @@
+package recursion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PermutationWithCaseChange {
+
+    static List<String> result = new ArrayList<>();
+    static List<String> permutationWithCaseChange(String in, String out){
+        if ((in.isEmpty())){
+            result.add(out);
+            return result;
+        }
+        String out1 = out;
+        String out2 = out;
+        if(Character.isAlphabetic(in.charAt(0)) ){
+            out2 += (in.toUpperCase().charAt(0));
+            out1 += (in.toLowerCase().charAt(0));
+            in = in.substring(1);
+            permutationWithCaseChange(in,out1);
+            permutationWithCaseChange(in,out2);
+        }
+        else{
+            out+=in.charAt(0);
+            in=in.substring(1);
+            permutationWithCaseChange(in,out);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(permutationWithCaseChange("a1B2",""));
+    }
+}
