a385018e28934db23d06d2a087337eeff20a8e7e
saurabhsingh <saurabh623a@gmail.com>
2024-01-31 22:13:01 +0530

    recursion added
    Signed-off-by: saurabhsingh <saurabh623a@gmail.com>

---
+++ pepcoding/PrintIncreasing.java
@@ -0,0 +1,15 @@
+package pepcoding;
+
+public class PrintIncreasing {
+    public static void main(String[] args) {
+        printIncreasing(5);
+
+    }
+    public static void printIncreasing(int n) {
+        if (n == 0) {
+            return;
+        }
+        printIncreasing(n - 1);
+        System.out.println(n);
+    }
+}
---
+++ pepcoding/SortAnArray.java
@@ -0,0 +1,13 @@
+package pepcoding;
+
+public class SortAnArray {
+    public static void main(String[] args) {
+
+    }
+    public static void sort(int[] nums, int n) {
+        if (nums.length == 0){
+            return ;
+        }
+        System.out.println(nums);
+    }
+}
